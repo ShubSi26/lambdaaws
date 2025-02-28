e953bb185f9d0a1c5fed8b4aa14dce536d0c4d07
Shubham Singh <112804969+ShubSi26@users.noreply.github.com>
2025-03-01 00:27:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,2 @@
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": "Hello from Lambdaaaaaaaaaa7uyjhgtregdfhnaaaaaaaa!"}
+    return {"statusCode": 200, "body": "Hello from ko!"}
